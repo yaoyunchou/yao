be6e9f3413f405ca94c393e8779168c4f7ef6423
yao <303630573@qq.com>
2015-09-06 20:15:13 +0800

    最新晚上最后的更新

--- html/js/slider.js
+++ html/js/slider.js
@@ -3,38 +3,47 @@
  */
 (function(window,underfind){
     function yao(a) {
-            new fun(a);
+        new fun(a);
     }
-          var fun;
-          yao.version = "1.0", yao.author = "yaoyunchou";
-          fun=function (a){
-             this.config=a||{};
+    var fun;
+    yao.version = "1.0", yao.author = "yaoyunchou";
+    fun=function (a){
+        this.config=a||{};
+        yao.version=this.config.version?this.config.version: yao.version;
+        this.render();
+    },fun.prototype.run=function(){
+        //console.log(this.config.run());
 
-        },fun.prototype.run=function(){
-             console.log("I can fly!");
-         },fun.prototype.rt=function(){
-             var c = this.config;
-             console.log(c.page);
-         }
+        console.log("我是原版"+yao.version);
+
+
+    },fun.prototype.rt=function(){
+        var c = this.config;
+        console.log(c.page);
+    },fun.prototype.render=function(a){
+        //this.config.run typeof function?;
+        // typeof(this.config.run)== "function";
+        typeof(this.config.run) === "function"?this.config.run():this.run();
+    }
 
 
     "function" == typeof define ? define(function() {
         return yao;
+
     }) : "undefined" != typeof exports ? module.exps = yao : window.yao = yao;
- /*   var yao = window.yao={
-           isCur:0,
+    /*   var yao = window.yao={
+     isCur:0,
 
 
-          mov:function(){},
+     mov:function(){},
 
-         doit:function(){
-             console.log(this.isCur);
+     doit:function(){
+     console.log(this.isCur);
 
-         }
-    }
-*/
+     }
+     }
+     */
 })(window);
 
-console.log(yao({"page":100}).rt(a));
 
 
